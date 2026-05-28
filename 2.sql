33f314c01f8b26c9c68d809e03d6410aa8601f9e
Getabosi <timitimie@yandex.ru>
2026-05-28 17:48:30 +0300

    2

---
+++ 2.sql
@@ -0,0 +1,20 @@
+SELECT title, duration
+FROM track
+ORDER BY duration DESC
+LIMIT 1;
+
+SELECT title, duration
+FROM track
+WHERE duration >= 210;
+
+SELECT title, release_year
+FROM compilation
+WHERE release_year BETWEEN 2018 AND 2020;
+
+SELECT name
+FROM artist
+WHERE name NOT LIKE '% %';
+
+SELECT title
+FROM track
+WHERE LOWER(title) LIKE '%my%' OR LOWER(title) LIKE '%мой%';
